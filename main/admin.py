5c1413f3268c00dad9ecc3f664451d63d5c5a068
raneem32283 <alamal.nemonem@gmail.com>
2024-06-21 20:02:55 +0300

    r

--- main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import City
-admin.site.register(City)
-
-# Register your models here.
--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-
-    path('',views.index, name='index'),
-    # path('',views.run_algorithms, name='run_algorithms'),
-    # path('',views.your_view, name='your_view'),
-    # path('',views.your_endpoint, name='your_endpoint'),
-]
